c43301d069fcf11fb6141f9ec6d3f528bca2320a
masanarih0ri <masanarihoridevelop@gmail.com>
2022-03-07 08:05:16 +0900

    add markdown

--- promise_sample.js
+++
@@ -1,8 +0,0 @@
-try {
-  const result = doSomething()
-  const newResult = doSomethingElse(result)
-  const finalResult = doThirdRhing(newResult)
-  console.log(`Got the final result: ${finalResult}`)
-} catch (error) {
-  failureCallback(error)
-}
